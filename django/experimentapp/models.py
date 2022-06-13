0f970af3e1286c261966665b6dfcda27929ab492
dvegrod <dvegrodu@gmail.com>
2022-06-13 20:37:37 +0200

    c init

--- django/experimentapp/models.py
+++ django/experimentapp/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+
+class Domain(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=128, default='Unnamed domain')
+    description = models.CharField(max_length=1024, null=True)
+
+class Subdomain(models.Model):
+    id = models.AutoField(primary_key=True)
+    domain = models.ForeignKey(Domain, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, default='Unnamed subdomain')
+    description = models.CharField(max_length=1024, null=True)
+
+class AARM(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    sub_domain = models.ForeignKey(Subdomain, on_delete=models.CASCADE)
+    name = models.CharField(max_length=128, default='Unnamed AARM')
+    description = models.CharField(max_length=1024, null=True)
+
+class RawArmDataRegister(models.Model):
+    aarm = models.ForeignKey(AARM, on_delete=models.CASCADE)
+    date = models.DateField()
+    price = models.FloatField()
+    additional = models.JSONField(null=True)
+
+class AbstractRewardRegister(models.Model):
+    aarm = models.ForeignKey(AARM, on_delete=models.CASCADE)
+    date = models.DateField()
+    reward = models.FloatField()
+
+class Experiment(models.Model):
+    id = models.AutoField(primary_key=True)
