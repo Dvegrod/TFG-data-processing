55d01f26285140671ef3fc67a41c21fe89204065
Daniel Sergio Vega Rodríguez <dvegrodu@gmail.com>
2022-06-14 00:56:27 +0200

    Comm

--- django/agentwebsite/urls.py
+++ django/agentwebsite/urls.py
@@ -15,9 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from experimentapp.views import dashboard
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.dashboard, name='dashboard'),
+    path('', dashboard, name='dashboard'),
 ]
